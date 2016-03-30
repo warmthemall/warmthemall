95ad224745ab19bf6392d4fe0a70ec9919e64a8e
Warm Them All <warmthemallqu@gmail.com>
2016-03-30 13:17:16 +0000

    Add user authentication

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, include, url
 from .views import *
 
+
+
+
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),
+    url(r'^user/', include('registration.backends.simple.urls')),
+    url(r'^user/', include('django.contrib.auth.urls')),
 )
